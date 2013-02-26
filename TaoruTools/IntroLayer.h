c6d8d27ff94302715a378430735cf2ad37050a34
taoru <taorulab@gmail.com>
2013-02-27 00:36:47 +0900

    Initial Commit

---
+++ TaoruTools/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TaoruTools
+//
+//  Created by Shintaro Taya on 13/02/27.
+//  Copyright taoru 2013年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TaoruTools/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TaoruTools
+//
+//  Created by Shintaro Taya on 13/02/27.
+//  Copyright taoru 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
